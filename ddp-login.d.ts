4fabf780e93653c4824483ae5aead57f73836644
Osei Fortune <fortune.osei@yahoo.com>
2016-07-14 03:04:01 -0400

    Clean up & added typings

--- ddp-login.d.ts
+++ ddp-login.d.ts
@@ -1,7 +1,4 @@
 export declare class DDPLogin {
-    private plaintextToDigest(pass:String);
-    private isEmail(addr:String);
-    private attemptLogin(ddp, user, pass, options, cb);
     loginWithUsername(dpp, username: String, password: String, cb: Function): any;
     loginWithEmail(dpp, email: String, password: String, cb: Function): any;
     loginWithAccount(dpp, emailOrUsername: String, password: String, cb: Function): any;
